89e310ab59c2013ccc2b1c4163c77c072ca1bd3a
Aleksandr K <adeskmath@gmail.com>
2020-08-09 01:44:24 +0300

    MyFirst start

---
+++ src/MyFirstPackage/HelloWorldClass.java
@@ -0,0 +1,7 @@
+package MyFirstPackage;
+
+public class HelloWorldClass {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/MyFirstPackage/Lesson2.java
@@ -0,0 +1,10 @@
+package MyFirstPackage;
+//T3.2 Тест блока 2
+public class Lesson2 {
+    static double fractional(double num){
+        return num%1;
+    }
+    public static void main(String[] args) {
+        System.out.println(fractional(1.53));
+    }
+}
